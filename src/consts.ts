7e791a978dc479aac82badf770b27d4b5ededf19
S-andres0694 <sebasandres0694@gmail.com>
2025-03-26 13:00:05 +0000

    Enhance site styling and dark mode support
    - Added new constants for dark and light mode colors in `consts.ts`.
    - Updated `BlogLogo` component to support dark mode colors.
    - Modified `Header` component to include dark mode background styling.
    - Improved global CSS with dark mode styles for body, main content, and code elements.
    - Cleaned up and organized CSS for better readability and maintainability.

--- src/consts.ts
+++ src/consts.ts
@@ -7,3 +7,7 @@ export const SITE_LOGO_COLOR: string = '#001eff';
 export const SITE_LINKEDIN_URL: string =
 	'https://www.linkedin.com/in/sebastian-el-khoury-seoane-234791303/';
 export const SITE_GITHUB_URL: string = 'https://github.com/S-andres0694';
+export const DARK_MODE_BACKGROUND_COLOR: string = '#121212';
+export const DARK_MODE_ANCHOR_LINK_COLOR: string = '#fb7ae3';
+export const LIGHT_MODE_BACKGROUND_COLOR: string = '#fafafa';
+export const LIGHT_MODE_ANCHOR_LINK_COLOR: string = '#2337ff';
